75ef54f20222f2bf9db94986b82b628655c1372f
178308360 <178308360@qq.com>
2021-04-15 22:26:01 +0800

    first commit

--- 10_learn_react-transition-group/src/index.js
+++ 10_learn_react-transition-group/src/index.js
@@ -6,6 +6,7 @@ import 'antd/dist/antd.css';
 
 import './transition/CSSTransition.css'
 import './transition/Switch.css'
+import './transition/TransitionGroup.css'
 
 ReactDOM.render(
   <React.StrictMode>
--- 10_learn_react-transition-group/src/transition/TransitionGroupDemo.js
+++ 10_learn_react-transition-group/src/transition/TransitionGroupDemo.js
@@ -12,24 +12,37 @@ export default class TransitionGroupDemo extends PureComponent {
   
   render() {
     return (
-      <div>
+      <TransitionGroup>
        {
          this.state.names.map((item,index)=>{
            return (
-             <CSSTransition key={index}>
-             <div >{item}</div>
+             <CSSTransition key={item}
+             timeout={300}
+             classNames='item'
+             >
+
+             <div >{item}
+             <button onClick={e=>this.removeItem(index)}>-</button>
+             </div>
              </CSSTransition>
            )
          })
        }
        <button onClick={e=>this.addName()}>+</button>
-      </div>
+      </TransitionGroup>
 
     )
   }
   addName(){
     this.setState({
       names:[...this.state.names,'code']
+    })
+  }
+  removeItem(index){
+    const newNames=[...this.state.names];
+    newNames.splice(index,1)
+    this.setState({
+      names:newNames
     })
   }
 }
